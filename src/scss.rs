a94db33b5bb2931ad3d2add51470c9dcc4018475
andreeich <andreei.chervoniak@icloud.com>
2025-07-22 22:03:17 +0300

    Bump version to 0.1.2 and remove SomeSass LSP support

--- src/scss.rs
+++ src/scss.rs
@@ -2,19 +2,15 @@ mod language_servers;
 
 use zed_extension_api::{self as zed, LanguageServerId, Result};
 
-use crate::language_servers::{SCSSLsp, SomeSass};
+use crate::language_servers::SCSSLsp;
 
 struct SCSSExtension {
     scss_lsp: Option<SCSSLsp>,
-    some_sass: Option<SomeSass>,
 }
 
 impl zed::Extension for SCSSExtension {
     fn new() -> Self {
-        Self {
-            scss_lsp: None,
-            some_sass: None,
-        }
+        Self { scss_lsp: None }
     }
 
     fn language_server_command(
@@ -22,19 +18,15 @@ impl zed::Extension for SCSSExtension {
         language_server_id: &LanguageServerId,
         worktree: &zed::Worktree,
     ) -> Result<zed::Command> {
-        match language_server_id.as_ref() {
-            SCSSLsp::LANGUAGE_SERVER_ID => {
-                let intelephense = self.scss_lsp.get_or_insert_with(|| SCSSLsp::new());
-                intelephense.language_server_command(language_server_id, worktree)
-            }
-            SomeSass::LANGUAGE_SERVER_ID => {
-                let some_sass = self.some_sass.get_or_insert_with(|| SomeSass::new());
-                some_sass.language_server_command(language_server_id, worktree)
-            }
-            language_server_id => Err(format!("unknown language server: {language_server_id}")),
+        if language_server_id.as_ref() != SCSSLsp::LANGUAGE_SERVER_ID {
+            return Err(format!("unknown language server: {language_server_id}"));
         }
+
+        let scss_lsp = self.scss_lsp.get_or_insert_with(|| SCSSLsp::new());
+        scss_lsp.language_server_command(language_server_id, worktree)
     }
 }
+
 zed::register_extension!(SCSSExtension);
 
 /// Extensions to the Zed extension API that have not yet stabilized.
